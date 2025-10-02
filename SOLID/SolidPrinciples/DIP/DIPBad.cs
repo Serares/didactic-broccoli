43036aeaafd9945d7415298feb5fbdf49eccb2b3
Rares <>
2025-10-02 13:25:55 +0300

    🌐 Dependency Inversion Principle

---
+++ SOLID/SolidPrinciples/DIP/DIPBad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.DIP
+{
+    internal class DIPBad
+    {
+
+        /**
+         *  Here CustomerService directly depends on the concrete CustomerRepository
+         *  Violating the DIP principle
+        */
+        public class CustomerRepository
+        {
+            public void Add(Customer customer)
+            {
+
+            }
+
+            public class CustomerService
+            {
+                private readonly CustomerRepository _customerRepository = new();
+
+                public void RegisterCustomer(Customer customer)
+                {
+                    _customerRepository.Add(customer);
+                }
+            }
+        }
+    }
+}
---
+++ SOLID/SolidPrinciples/DIP/DIPExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Dependency Inversion Principle
+ * A high-level module should not depend on a low-level module
+ * Both should depend on abstractions
+ * CustomerService  
+*/
+namespace SolidPrinciples.DIP
+{
+    internal class DIPExample
+    {
+        
+
+        public interface ICustomerRepository
+        {
+            void Add(Customer customer);
+        }
+
+        public class CustomerRepository : ICustomerRepository
+        {
+            public void Add(Customer customer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class CustomerService
+        {
+            private readonly ICustomerRepository _customerRepository;
+
+            public CustomerService(ICustomerRepository customerRepository)
+            {
+                _customerRepository = customerRepository;
+            }
+
+            public void RegisterCustomer(Customer customer)
+            {
+                _customerRepository.Add(customer);
+            }
+        }
+    }
+}
