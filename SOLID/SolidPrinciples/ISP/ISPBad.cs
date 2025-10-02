b0531136dd7eb270c64564f0b891d5b6b425a6c0
Rares <>
2025-10-02 12:52:27 +0300

    🧪 isp

---
+++ SOLID/SolidPrinciples/ISP/ISPBad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ❗ less flexible system design
+// this interface forces clients to implement all methods
+// even if one client needs just the Print method
+namespace SolidPrinciples.ISP
+{
+    internal class ISPBad
+    {
+        public interface IMultiFunctionDevice
+        {
+            void Print(string message);
+            void Scan(string message);
+            void Fax(string message);
+        }
+
+        public class MultiFunctionPrinter : IMultiFunctionDevice
+        {
+            public void Fax(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(string message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ SOLID/SolidPrinciples/ISP/ISPExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Interface Segregation Principle
+// - segregate functionalitites intor separate interfaces
+// - ensure clients that only need printing don't depend on scanning or faxing methods
+namespace SolidPrinciples.ISP
+{
+    internal class ISPExample
+    {
+        public interface IPrinter
+        {
+            void Print(string d);
+        }
+
+        public interface IScanner
+        {
+            void Scan(string d);
+        }
+
+        public interface IFax
+        {
+            void Fax(string d);
+        }
+
+        public class MultiFunctionPrinter : IPrinter, IScanner, IFax
+        {
+            public void Fax(string d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print(string d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(string d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
