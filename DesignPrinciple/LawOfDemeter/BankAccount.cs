7643462c9675fa284991df96a4fee4ea05911700
Rares <>
2025-10-02 17:40:56 +0300

    🖼️ LoD design principle

---
+++ DesignPrinciple/LawOfDemeter/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawOfDemeter
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; set; }
+        public BankAccount(decimal initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ DesignPrinciple/LawOfDemeter/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LawOfDemeter
+{
+    /**
+     *  A module should only have knowledge of and talk
+     *  directly to closely related modules
+    */
+    public class Customer
+    {
+        private BankAccount account;
+        // The wallet behavior is violating the LoD principle
+        public Wallet Wallet { get; set; }
+        public Customer(BankAccount account)
+        {
+            this.account = account;
+        }
+
+        public decimal CheckAccountBalance()
+        {
+            // ❗ accessing balance through Wallet and Account
+            // create tight coupling
+            // return Wallet.Account.Balance;
+            return account.GetBalance();
+        }
+    }
+
+    public class Wallet
+    {
+        public BankAccount? Account { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
